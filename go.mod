9c12c29cb0a4008011d3607a2e85f17cbfa6e882
Edson Tofolo <edsontofolo@gmail.com>
2023-03-21 20:18:23 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EdsonGustavoTofolo/desafio-multithreading-golang
+
+go 1.20
---
+++ internal/usecase/get_cep.go
@@ -0,0 +1,9 @@
+package usecase
+
+type GetCep struct {
+	Cep string
+}
+
+func (c GetCep) Execute() {
+
+}
